7069a72fd03ae8d44e8be793d5c6cb82d16c4a96
rio <sw_park0205@naver.com>
2023-04-04 21:38:05 +0900

    간단한 오류 수정

---
+++ src/main/java/com/kgitbank/megakgcoffee/Controller/Find/FindController.java
@@ -0,0 +1,15 @@
+package com.kgitbank.megakgcoffee.Controller.Find;
+
+import com.kgitbank.megakgcoffee.Opener.Opener;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FindController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
